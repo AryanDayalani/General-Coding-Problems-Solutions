d3942f60ecd3a1bbe445f050f8bfdbf17f5cc1d5
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2025-12-06 02:22:55 +0530

    Day 4 - Implemented the code to Merge the overlapping intervals of an array

---
+++ DSA_GCR/Day4/RA2311003010836.java
@@ -0,0 +1,20 @@
+package Day4;
+// This is the Day - 4 of the GCR Problems. Merge Overlapping Intervals in an array.
+
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int k = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[k][1] >= intervals[i][0]) {
+                intervals[k][1] = Math.max(intervals[k][1], intervals[i][1]);
+            } else {
+                k++;
+                intervals[k] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(intervals, 0, k + 1);
+    }
+}
