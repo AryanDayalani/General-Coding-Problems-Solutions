7c8ba5af216cc526789527b032a4ce1da83446cf
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-02-09 21:26:11 +0530

    DFS on graphs using recursion in Java

--- DFS.java
+++ DFS.java
@@ -1,13 +1,13 @@
-// Java implementation of DFS
+// Java implementation of DFS using recursion
 
 import java.util.*;
 
 public class DFS {
     public static void dfs(int node, boolean[] visited, List<List<Integer>> graph) {
-        visited[node] = true;           // Visit a node
-        System.out.print(node + " ");   // Print it out
+        visited[node] = true; // Visit a node
+        System.out.print(node + " "); // Print it out
 
-        for (int neighbour : graph.get(node)) {     // For every neighbour, if it is unvisited, recursiely DFS on it
+        for (int neighbour : graph.get(node)) { // For every neighbour, if it is unvisited, recursiely DFS on it
             if (!visited[neighbour]) {
                 dfs(neighbour, visited, graph);
             }
