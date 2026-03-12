e77dfc19cd769f2594382f08dc633bd03ee364e5
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-03-12 19:29:27 +0530

    Updated to handle no-name edge-case

--- UsernameGenerator.java
+++ UsernameGenerator.java
@@ -3,17 +3,21 @@ import java.util.*;
 public class UsernameGenerator {
     public String generateUsername(String name, int year) {
         // For checking edge case 1 of year out of bounds
+        boolean namePresent = true;
         if (year < 1000 || year > 10000) {
             return "Invalid Year";
         }
         if (name.isEmpty()) {
             System.out.println("Please enter a valid name!");
+            namePresent = false;
         }
 
         String firstName = name.trim().split(" ")[0].toLowerCase();
         String lastTwoDigits = String.valueOf(year).substring(2);
-
+        if (namePresent) {
         return firstName + lastTwoDigits;
+        }
+        else return "";
     }
 
     public static void main(String[] args) {
