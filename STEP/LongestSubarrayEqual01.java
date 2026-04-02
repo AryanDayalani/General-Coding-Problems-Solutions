e1f18fa106a7ef3cb38bc0ae1ab86a0fe73083af
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-04-02 19:57:31 +0530

    Optimal approach to find maximum length and start and end indices of contiguous subarray having equal 0s and 1s

--- STEP/LongestSubarrayEqual01.java
+++ STEP/LongestSubarrayEqual01.java
@@ -27,6 +27,36 @@ public class LongestSubarrayEqual01 {
         return maxLen;
     }
 
+    public static int[] findMaxLengthWithIndices(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int maxLen = 0;
+        int start = 0, end = 0;
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                sum -= 1;
+            } else {
+                sum += 1;
+            }
+
+            if (map.containsKey(sum)) {
+                int len = i - map.get(sum);
+                if (len >= maxLen) {
+                    maxLen = len;
+                    start = map.get(sum) + 1;
+                    end = i;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return new int[] { start, end, maxLen };
+    }
+
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
@@ -39,7 +69,10 @@ public class LongestSubarrayEqual01 {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println("Result: " + findMaxLength(arr));
+        int[] result = findMaxLengthWithIndices(arr);
+        System.out.println("Start Index: " + result[0]);
+        System.out.println("End Index: " + result[1]);
+        System.out.println("Max Length: " + result[2]);
         sc.close();
     }
 }
