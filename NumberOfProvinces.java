9c7ab84a2c57342585d446fe05357726b0f6bb8a
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-06-19 11:06:47 +0530

    FIndind the number of connected nodes (direct/indirect)

---
+++ NumberOfProvinces.java
@@ -0,0 +1,43 @@
+// Leetcode problem 547 that finds the number of provinces (group of directly/indirectly connected cities)
+import java.util.*;
+
+
+class FindProvince {
+    public int findCircleNum(int[][] isConnected) {
+        boolean[] visited = new boolean[isConnected.length];
+        int provinces = 0;
+
+        for (int i = 0; i<isConnected.length; i++) {
+            if(!visited[i]) {
+                provinces++;
+                dfs(i, visited, isConnected);
+            }
+        }
+
+        return provinces;
+    }
+
+    public void dfs(int node, boolean[] visited, int[][] graph) {
+        visited[node] = true;
+        for (int neighbour = 0; neighbour < graph.length; neighbour++) {
+            if (!visited[neighbour] && graph[node][neighbour] == 1) {
+                dfs(neighbour, visited, graph);
+            }
+        }
+    }
+}
+
+public class NumberOfProvinces {
+
+    public static void main(String[] args) {
+        int[][] isConnected = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
+
+        FindProvince solution = new FindProvince();
+        int result = solution.findCircleNum(isConnected);
+        System.out.println("Number of provinces: " + result);
+    }
+}
