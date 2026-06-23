20dd939e1c9326cdbf468b79b49f35dcc6a0d0ec
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-06-23 10:52:26 +0530

    The TwoSum problem solution using Hashmap in Java

--- TwoSum.java
+++ TwoSum.java
@@ -4,17 +4,21 @@ import java.util.HashMap;
 class TwoSummer {
     public int[] twoSum(int[] nums, int target) {
 
+        // Hashmap to store the values and indices
         HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Final result array
         int[] result = new int[2];
 
+        // For each number in list,
         for (int i = 0; i<nums.length; i++) {
-            int diff = target - nums[i];
-            if (map.containsKey(diff)) {
+            int diff = target - nums[i];    // find the diff between number and the target
+            if (map.containsKey(diff)) {    // if the diff is already present as a key in map, add the index and the value of the diff key into the result array
                 result[0] = i;
                 result[1] = map.get(diff);
                 return result;
              }
-             map.put(nums[i], i);
+             map.put(nums[i], i);   // else, add the number at the ith index as a key and the index 'i' as a value to the map
         }
         return result;
     }
