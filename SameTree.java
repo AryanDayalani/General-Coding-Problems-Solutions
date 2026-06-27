8e2f20939a574800ddabf3bae944c80ac13f1087
Aryan Naresh Dayalani <aryandayalani@gmail.com>
2026-06-27 12:37:31 +0530

    Finding whether given 2 Binary Trees are identical

--- SameTree.java
+++ SameTree.java
@@ -15,25 +15,34 @@ class TreeNode {
 
 class SameTreeChecker {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // both null, then true
-        if (p==null && q==null) {
+
+        // if both are empty trees, both are identical
+        if (p == null && q == null) {
             return true;
         }
-        // if either of them null & other non-null, then false
-        if (p!= null && q==null) {
+
+        // If only one of them is empty, can't be identical
+        if (p == null || q == null) {
             return false;
         }
-        if (p==null && q!=null) {
-            return false;
-        }
-        // if both have values, recursively check for each value
+
+        boolean l = false;  // stores if leftsubtree of p == q
+        boolean r = false;  // // stores if rightsubtree of p == q
+
+        // if value of p and q are same, we recursively check at both the left and right subtrees of p & q
         if (p.val == q.val) {
-        boolean left = isSameTree(p.left, q.left);
-        boolean right = isSameTree(p.right, q.right);
-        return (left && right);
+            l = isSameTree(p.left, q.left);
+            r = isSameTree(p.right, q.right);
+
         }
-        // if both values not same, false
-        return false;
+
+        // if both right and left subhalves identical, entire tree is identical
+        if (l && r) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
